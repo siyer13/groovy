c4eee0f16f570d14954d6c3ca401c547a969a046
Sridhar <sreedharriyer@gmail.com>
2016-08-05 21:07:16 -0700

    xml generation

--- Weather.groovy
+++ Weather.groovy
@@ -1,5 +1,20 @@
 @Grab('mysql:mysql-connector-java:5.1.25')
 @GrabConfig(systemClassLoader = true)
 import groovy.sql.Sql
-def sql = groovy.sql.Sql.newInstance('jdbc:mysql://localhost:3306/WEATHERINFO', 'root', 'password','com.mysql.jdbc.Driver')
+def sql = groovy.sql.Sql.newInstance('jdbc:mysql://localhost:3306/WEATHERINFO', 'root', '','com.mysql.jdbc.Driver')
 println sql.connection.catalog
+println "City Temperature"
+sql.eachRow('SELECT * from WEATHER') {
+printf "%-20s%s\n", it.CITY, it[1]
+}
+
+bldr = new groovy.xml.MarkupBuilder()
+
+bldr.WEATHER {
+  sql.eachRow('SELECT * FROM WEATHER') {
+    CITY(name: it.CITY, temperature: it.TEMEPERATURE)
+  }
+}
+
+
+dataSet = sql
